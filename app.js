61684caedf4bc4640703d55695b953f49e560660
BAN1337 <joaocleber.jc10@gmail.com>
2024-06-12 23:03:50 -0300

    Atualização de códigos - Aula 325

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const path = require('path')
-
 const checkListRouter = require('./src/routes/checklist')
 const rootRouter = require('./src/routes/index')
 require('./config/database')
-
+const methodOverride = require('method-override')
 const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
